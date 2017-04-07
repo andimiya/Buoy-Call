61a17a516041b0ab1002b77085f3899a2435745e
Andrea <takamiya.andrea@gmail.com>
2017-04-06 22:15:56 -1000

    Script to read through buoy URLs and extract files

--- scripts/pull-noaa.sh
+++ scripts/pull-noaa.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-yearstart=2000
+yearstart=2014
 yearend=2017
 buoyidstart=51208
 buoyidend=51211
-url=http://www.ndbc.noaa.gov/view_text_file.php\?filename=$buoyidstart h2016.txt.gz\&dir=data/historical/stdmet/
 
 for buoy in $(seq $buoyidstart $buoyidend); do
   for year in $(seq $yearstart $yearend); do
-    echo "hello-${buoy}-${year}-world"
+    curl -o buoy${buoy}-${year} http://www.ndbc.noaa.gov/view_text_file.php\?filename=${buoy}h${year}.txt.gz\&dir=data/historical/stdmet/
+    firstLine=$(head -n 1 buoy${buoy}-${year})
+    if [ "$firstLine" = "Unable to access data file" ]
+      then rm buoy${buoy}-${year};
+    fi
   done
 done
-
-test=
-
-curl -o buoydata $test
