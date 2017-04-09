17734a214127d23fea59790e6e8bf51e284ec4f3
Andrea <takamiya.andrea@gmail.com>
2017-04-08 15:55:11 -1000

    Replaces mm with month in header

--- scripts/raw2csv.sh
+++ scripts/raw2csv.sh
@@ -3,8 +3,10 @@
 # get the file
 # open in text editor?
 #
-# filename=buoy51208-2014
+filename=../data/buoy51208-2016
 # sed 's/ \{1,\}/,/g' $filename | awk 'NR != 2' | cut -d "#" -f 2| head
+# sed 's/'mm'/month/g' $filename |  awk 'NR != 2' | cut -d "#" -f 2| head
+
 
 yearstart=2015
 yearend=2016
@@ -16,7 +18,7 @@ for buoy in $(seq $buoyidstart $buoyidend); do
     filename=../data/buoy${buoy}-${year}
     # echo $filename
     csv=../data/csvs/buoy${buoy}-${year}.csv
-    
-    cat $filename | sed 's/ \{1,\}/,/g' | awk 'NR != 2' | cut -d "#" -f 2 > $csv
+
+    cat $filename | sed 's/ \{1,\}/,/g' | sed 's/'mm'/month/g' | awk 'NR != 2' | cut -d "#" -f 2 > $csv
   done
 done
