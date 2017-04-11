8f8d8d8cabc5692767c856864635e448986399e0
raaarwwwwandy <hirandysan@gmail.com>
2017-04-11 07:54:48 -1000

    implimented react and is serving connecting properly

---
+++ client/src/containers/app/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div className="App-header">
+          <h2>WATER WORLD</h2>
+        </div>
+       
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
