72495c2a6c1bc73778279f5cf1be57c25077f12b
raaarwwwwandy <hirandysan@gmail.com>
2017-04-11 23:41:53 -1000

    applied dev-tools, but modules is missing from it

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './containers/app/app.js';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import users from './reducers'; 
+import ReduxThunk from 'redux-thunk';
+import { createDevTools } from 'redux-devtools'
+
+let store = createStore(
+  users, 
+  window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_(), 
+  applyMiddleware(ReduxThunk)
+);
 
 ReactDOM.render(
+  <Provider store={store}>
   <App />,
+  </Provider>,
   document.getElementById('root')
 );
