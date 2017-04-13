075be3d3955440c0e39fd7b220148bc0dff047c4
Naoto Yamaguchi <naotoy@hawaii.edu>
2017-04-12 20:01:11 -1000

    Added bcrypt to creating user's passwords. Hashes password

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models'); 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const Users = db.User;
 
 router.route('/')
@@ -15,16 +17,23 @@ router.route('/')
   })
 
   .post( (req, res) =>{
-    console.log("REQ BODY:",req.body)
-    Users.create({
-      firstName: req.body.FirstName,
-      lastName: req.body.LastName,
-      email: req.body.Email,
-      password: req.body.Password
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(req.body.Password, salt, function(err, hash) {
+        Users.create({
+          firstName: req.body.FirstName,
+          lastName: req.body.LastName,
+          email: req.body.Email,
+          password: hash
+        })
+        .then((users) =>{
+          res.json(users);
+        })
+        .catch(err => {
+          console.log("error goes here I.E. USER ALREADY EXISTS", err);
+          //redirect to making user again and/or flash the error message
+        })
+      })
     })
-    .then((users) =>{
-      res.json(users);
-    });
   })
 
   module.exports = router; 
