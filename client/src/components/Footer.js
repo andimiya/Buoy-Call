5182ae75890a376b65fa1d3f45aef1cf053d2991
raaarwwwwandy <hirandysan@gmail.com>
2017-04-18 22:06:07 -1000

    footer is added

---
+++ client/src/components/Footer.js
@@ -0,0 +1,22 @@
+/* jshint esversion:6 */
+
+import React from 'react';
+
+import './Footer.css';
+
+class Footer extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="Footer">
+      <h1>FOOTER BAR</h1>
+      </div>
+    )
+  }
+
+}
+
+export default Footer;
