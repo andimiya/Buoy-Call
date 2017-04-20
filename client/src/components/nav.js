2b5e777677762eea13401b70af61e7244ccf493c
raaarwwwwandy <hirandysan@gmail.com>
2017-04-19 21:08:14 -1000

    added nav to branch

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './nav.css';
 
 const Nav = () => {
   return (
+    <div className="nav">
     <ul>
-        <li><Link to="/secret">secret route / only if youre logged in</Link></li>
         <li><Link to="/">Homepage</Link></li>
-        <li><Link to="/login">Login Page</Link></li>
+        <li><Link to="/login">Login To Account</Link></li>
         <li><Link to="/NewUser">Create an Account</Link></li>
+        <li><Link to="/secret">secret route / only if youre logged in</Link></li>
     </ul>
+    </div>
   )
 }
 
