a38ae3c8b17464c18287b518c3a5c25e6d06f937
raaarwwwwandy <hirandysan@gmail.com>
2017-04-22 09:23:34 -1000

    started modifying homepage route

--- client/src/containers/app/app.js
+++ client/src/containers/app/app.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import NewUser from '../../components/NewUser.js';
 import Checkout from '../../components/StripeCheckout';
+import Login from '../../components/Login.js';
 import users from '../../reducers/';
 import { createStore } from 'redux';
 import { connect } from 'react-redux';
 import { addUser } from '../../actions';
 import { addUserToState } from '../../actions';
-<<<<<<< HEAD
+
 import Footer from '../../components/Footer.js';
-=======
+
 import './app.css';
->>>>>>> development
 
 class App extends Component {
   constructor(props) {
