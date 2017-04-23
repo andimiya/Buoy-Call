8ac9838747bace51360f70dcb71e290adf1d542f
Naoto Yamaguchi <naotoy@hawaii.edu>
2017-04-22 19:43:46 -1000

    initial commit, have a working Hello User based on state. Some unwanted delay

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,17 +1,84 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addUserToState } from '../actions';
 
-const Nav = () => {
-  return (
-    <ul>
-        <li><Link to="/secret">secret route / only if youre logged in</Link></li>
-        <li><Link to="/">Homepage</Link></li>
-        <li><Link to="/login">Login Page</Link></li>
-        <li><Link to="/NewUser">Create an Account</Link></li>
-        <li><Link to="/Graph">Sample Graph!</Link></li>
-        <li><Link to="/Payment">Adopt a Buoy/Shark With Ca$h Money</Link></li>
-    </ul>
-  )
+class Nav extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  xhrLoginCheck(){
+    return new Promise(function(resolve,reject){
+      function reqListener(){
+        resolve(this.responseText);
+      }
+      let oReq = new XMLHttpRequest();
+      oReq.open('POST', '/api/users/checkLogin');
+      oReq.setRequestHeader('Content-type', 
+        'application/json')
+      oReq.addEventListener("load", reqListener)
+      oReq.send()
+    })
+  }
+
+  componentDidMount(){
+    this.xhrLoginCheck()
+    .then((userData)=>{
+      console.log(this.props)
+      let user = JSON.parse(userData)
+      this.props.onAddUser(user.id, user.firstName, user.lastName, user.email)
+    })
+    .catch(function(err){
+      console.log("component will mount error",err)
+    })
+  }
+
+  render(){
+    console.log("YAY", this.props.loggedInUser)
+    if(this.props.loggedInUser){
+      return(
+        <div>
+        <h3>hello {this.props.loggedInUser.firstName}!</h3>
+        <ul>
+          <li><Link to="/secret">secret route / only if youre logged in</Link></li>
+          <li><Link to="/">Homepage</Link></li>
+          <li><Link to="/login">Login Page</Link></li>
+          <li><Link to="/NewUser">Create an Account</Link></li>
+          <li><Link to="/Graph">Sample Graph!</Link></li>
+          <li><Link to="/Payment">Adopt a Buoy/Shark With Ca$h Money</Link></li>
+        </ul>
+      </div>
+      )
+    } else {
+      return(
+        <div>
+        <h3>hello you are not logged in!!</h3>
+        <ul>
+          <li><Link to="/secret">secret route / only if youre logged in</Link></li>
+          <li><Link to="/">Homepage</Link></li>
+          <li><Link to="/login">Login Page</Link></li>
+          <li><Link to="/NewUser">Create an Account</Link></li>
+          <li><Link to="/Graph">Sample Graph!</Link></li>
+          <li><Link to="/Payment">Adopt a Buoy/Shark With Ca$h Money</Link></li>
+        </ul>
+      </div>
+      )
+    }
+  }
 }
 
-export default Nav;
+
+const mapDispatchToProps = {
+  onAddUser: addUserToState
+}
+
+const mapStateToProps = (state) => {
+  console.log("STATE", state)
+  return {
+    loggedInUser: state.loggedInUser
+  }
+}
+
+export default connect(
+  mapStateToProps, mapDispatchToProps)(Nav)
