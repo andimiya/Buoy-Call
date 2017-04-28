f073342a8b02819d6bd5be029627f0a3541442f4
Andrea <takamiya.andrea@gmail.com>
2017-04-27 22:05:51 -1000

    Changed out to react-leaflet-markercluster, buoys are all mapped

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -1,63 +1,63 @@
 import React, {Component} from 'react';
-// import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-// import MarkerClusterGroup from 'react-leaflet-markercluster';
-// import places from '../lib/data.json';
-import {GeoJsonCluster} from 'react-leaflet-geojson-cluster';
-import request from 'superagent';
-// const geodata = JSON.stringify(places);
-
-const position = [-8.310,19.087];
-
-// const markers = [
-//   {lat: 49.8397, lng: 24.0297},
-//   {lat: 52.2297, lng: 21.0122},
-//   {lat: 51.5074, lng: -0.0901}
-// ];
+import { Map, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 class MapView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      places: null
+      markers: null
     };
-  }
-  componentWillMount() {
-    request
-      .get('http://localhost:8080/allbuoys')
-      .end(function(error, response){
-        if (error) return console.log(error);
 
-        this.setState({
-          places: response.body
-        });
-      }.bind(this));
+    this.getAllBuoys = this.getAllBuoys.bind(this);
+  }
+
+  getAllBuoys(){
+   return new Promise((resolve,reject) => {
+     function reqListener(){
+       resolve(JSON.parse(this.responseText));
+     }
+     let oReq = new XMLHttpRequest();
+     oReq.open('GET', `/allbuoys`);
+     oReq.setRequestHeader('Content-type',
+       'application/json')
+     oReq.addEventListener("load", reqListener)
+     oReq.send()
+   })
+  }
+
+  componentDidMount() {
+    this.getAllBuoys()
+    .then((markers) => {
+      this.setState({
+        markers: markers
+      })
+    })
   }
 
   render(){
-    const { places } = this.state;
-    if(!places){
+    const { markers } = this.state;
+    console.log(markers, 'data');
+
+    if(!markers){
       return (<div>loading...</div>);
     }
 
-    console.log('this.state', this.state.places.features[0].geometry.coordinates);
-
     return (
-      <div className="MapView">
-        <Map id="map"
+        <Map className="markercluster-map"
           style={{height: '900px'}}
           center={[-8.310,19.087]}
           zoom={1.5}
-          minzoom={0}
-          maxzoom={15}>
+          minZoom={0}
+          maxZoom={15}>
           <TileLayer
             url="https://api.mapbox.com/styles/v1/jonathonlaylo/cj1g01mw200062ss53ht46jgb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9uYXRob25sYXlsbyIsImEiOiJjajE3bDUwZ2YwNHhjMnFvN2cwaW5vYWFrIn0.ZYv3mfTj8HIP5LdLMWvw4Q"
-            attribution="<attribution>"
           />
-          <GeoJsonCluster data={places}/>
+          <MarkerClusterGroup
+            markers={markers}
+            wrapperOptions={{enableDefaultStyle: true}}
+    />
         </Map>
-      </div>
     );
   }
 }
