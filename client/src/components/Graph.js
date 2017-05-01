a5d39f5355d2e316c2dbe2736e8c377cde33bf98
Naoto Yamaguchi <naotoy@hawaii.edu>
2017-05-01 00:11:02 -1000

    removed border from graph

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -149,7 +149,7 @@ class Graph extends Component {
       <div>
 
         <AreaChart width={1750} height={250} data={this.props.graphState}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
           <defs>
             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
