c1f9b5ac0d31e7f8094a2d7a175354e6d0fed83c
Andrea <takamiya.andrea@gmail.com>
2017-05-01 23:11:37 -1000

    Returning shark data from the server onto the client

--- client/src/components/StripeCheckout.js
+++ client/src/components/StripeCheckout.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import config from '../../config';
 import { connect } from 'react-redux';
 import { addSharkToState } from '../actions';
 import '../containers/App/App.css';
 
-class Checkout extends React.Component {
+class Checkout extends Component {
 
   constructor(props) {
     super(props);
-      this.props.onAddSharkToState(this.props.match.params.shark_id);
+
+  }
+
+  getAllSharks(){
+    return new Promise((resolve,reject) => {
+      function reqListener(){
+        resolve(JSON.parse(this.responseText));
+      }
+      let oReq = new XMLHttpRequest();
+      console.log(this.props.mm, 'mm id');
+      console.log(this.props.shark_id, 'shark id');
+
+      oReq.open('GET', `/api/shark/${this.props.shark_id}`);
+      oReq.setRequestHeader('Content-type',
+       'application/json')
+      oReq.addEventListener("load", reqListener)
+      oReq.send()
+    })
+  }
+
+  componentDidMount(arr) {
+    Promise.all([
+      this.props.onAddSharkToState(this.props.match.params.shark_id)
+    ])
+    .then(() => {
+      this.getAllSharks()
+      .then((data) => {
+        console.log(data, 'shark data');
+      })
+    })
   }
 
   onToken = (token) => {
@@ -64,7 +93,8 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state) => {
   return {
-    shark_id: state.shark_id
+    shark_id: state.shark_id,
+    mm: state.mm
   }
 }
 
