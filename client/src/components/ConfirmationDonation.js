e9022c891dc1b873ab7bdff6343d20e626e1f93e
Andrea <takamiya.andrea@gmail.com>
2017-05-03 19:27:57 -1000

    Fixed a bug on the confirmation page that showed the wrong shark name. Also created a Donate page to fix the blank /adopt route

---
+++ client/src/components/ConfirmationDonation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ConfirmationDonation extends React.Component {
+  render() {
+    return (
+      <div className="confirmation-page">
+        <h1>You're awesome. Thank you for your contribution!</h1>
+        <br />
+        <p>Your donation will help save the ocean and the planet</p>
+      </div>
+    )
+  }
+}
+
+export default ConfirmationDonation;
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -27,7 +27,6 @@ class Nav extends Component {
   componentWillMount(){
     this.xhrLoginCheck()
     .then((userData)=>{
-      console.log(this.props)
       let user = JSON.parse(userData)
       this.props.onAddUser(user.id, user.firstName, user.lastName, user.email)
     })
@@ -54,7 +53,6 @@ class Nav extends Component {
     event.preventDefault();
     this.xhrLogOut()
     .then(()=>{
-      console.log("this.propS",this.props)
       this.props.onLogOut()
     })
   }
@@ -69,9 +67,10 @@ class Nav extends Component {
           </h1>
         </div>
           <ul className="main-nav">
-            <li><Link to="/Payment">Adopt</Link></li>
-            <li><Link to="/Graph">Historical Charts</Link></li>
-            <li><Link to="/">Non-Profit Support</Link></li>
+            <li><Link to="/">Adopt</Link></li>
+            <li><Link to="/donate">Donate</Link></li>
+            <li><Link to="/aboutus">About Us</Link></li>
+            <li><Link to="/contactus">Contact Us</Link></li>
           </ul>
 
           <div className="logged-in-note">
@@ -96,9 +95,10 @@ class Nav extends Component {
           </h1>
         </div>
         <ul className="main-nav">
-          <li><Link to="/Payment">Adopt</Link></li>
-          <li><Link to="/Graph">Historical Charts</Link></li>
-          <li><Link to="/">Non-Profit Support</Link></li>
+          <li><Link to="/">Adopt</Link></li>
+          <li><Link to="/donate">Donate</Link></li>
+          <li><Link to="/aboutus">About Us</Link></li>
+          <li><Link to="/contactus">Contact Us</Link></li>
         </ul>
         <ul className="login-nav">
           <li><Link to="/login">Log In</Link></li>
@@ -110,7 +110,6 @@ class Nav extends Component {
     }
   }
 }
-
 
 const mapDispatchToProps = {
   onAddUser: addUserToState,
