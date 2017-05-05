4485210830cb77fb37e6f3fecd1e5999edc97e73
jonathonlaylo <jonathon.laylo@gmail.com>
2017-05-04 20:35:26 -1000

    still working on the contactus component. handle states but not sending still

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -5,43 +5,93 @@ import { Router } from 'react-router';
 class ContactUs extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      Name: '',
+      Message: ''
+    }
+    this.handleName = this.handleName.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleName(event){
+    this.setState({
+      Name: event.target.value
+    });
+  }
+
+  handleMessage(event){
+    this.setState({
+      Message: event.target.value
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('this.state', this.state);
+    this.sendMail({
+      Name: this.state.Name,
+      Message: this.state.Message
+    });
+  }
+
+  sendMail(mail){
+    return new Promise(function(resolve, reject){
+      function reqListener(){
+        let results = JSON.parse(this.responseText);
+        resolve(results);
+      }
+      let oReq = new XMLHttpRequest();
+      oReq.open('POST', '/api/users/ContactUs');
+      oReq.setRequestHeader('Content-type', 'application/json');
+      oReq.send(JSON.stringify(mail));
+    })
   }
 
   render() {
     return (
       <div className="ContactUs-Container">
-        <h1>Contact Us</h1>
-        <form>
-
-          <div class="form-group">
-            <label class="control-label " for="name">Name</label>
-            <input class="form-control" id="name" name="name" type="text"/>
-          </div>
-
-          <div class="form-group">
-            <label class="control-label requiredField" for="email">Email<span class="asteriskField">*</span></label>
-            <input class="form-control" id="email" name="email" type="text"/>
-          </div>
-
-          <div class="form-group">
-            <label class="control-label " for="subject">Subject</label>
-            <input class="form-control" id="subject" name="subject" type="text"/>
-          </div>
-
-          <div class="form-group">
-            <label class="control-label " for="message">Message</label>
-            <textarea class="form-control" cols="40" id="message" name="message" rows="10"></textarea>
-          </div>
-
-          <div class="form-group">
-            <button class="btn btn-primary " name="submit" type="submit">Submit</button>
-          </div>
-        </form>
-
+        <h1>Contact Us</h1><br/>
+        <form id="myform" onsubmit="emailjs.sendForm('buoycall_info_gmail_com', 'send_email', this.id); return true;" method="post">
+          <label>Name</label><br/>
+          <input type="text" name="Name" onChange={this.handleName}/><br/><br/>
+          <label>Sending To</label><br/>
+          <input type="text" name="SendingTo" value="buoycall.info@gmail.com"/><br/><br/>
+          <label>Message</label><br/>
+          <input type="text" name="Message" onChange={this.handleMessage}/><br/>
+          <button>
+          Send
+          </button>
+          </form>
       </div>
     )
   }
 }
 
 export default ContactUs;
+
+        // <form>
+        //   <div class="form-group">
+        //     <label class="control-label " for="name">Name</label>
+        //     <input class="form-control" id="name" name="name" type="text"/>
+        //   </div>
+
+        //   <div class="form-group">
+        //     <label class="control-label requiredField" for="email">Email<span class="asteriskField">*</span></label>
+        //     <input class="form-control" id="email" name="email" type="text"/>
+        //   </div>
+
+        //   <div class="form-group">
+        //     <label class="control-label " for="subject">Subject</label>
+        //     <input class="form-control" id="subject" name="subject" type="text"/>
+        //   </div>
+
+        //   <div class="form-group">
+        //     <label class="control-label " for="message">Message</label>
+        //     <textarea class="form-control" cols="40" id="message" name="message" rows="10"></textarea>
+        //   </div>
+
+        //   <div class="form-group">
+        //     <button class="btn btn-primary " name="submit" type="submit">Submit</button>
+        //   </div>
+        // </form>
