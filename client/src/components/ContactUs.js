ece4cc18b44dfb703e4121f10fa5b285c6ae1c1f
jonathonlaylo <jonathon.laylo@gmail.com>
2017-05-04 15:06:36 -1000

    working example of email contact form on indexHtml, working on react

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router';
 
+
 class ContactUs extends React.Component {
   constructor(props) {
     super(props);
@@ -11,33 +12,33 @@ class ContactUs extends React.Component {
     return (
       <div className="ContactUs-Container">
         <h1>Contact Us</h1>
-<form>
+        <form>
 
-  <div class="form-group">
-    <label class="control-label " for="name">Name</label>
-    <input class="form-control" id="name" name="name" type="text"/>
-  </div>
+          <div class="form-group">
+            <label class="control-label " for="name">Name</label>
+            <input class="form-control" id="name" name="name" type="text"/>
+          </div>
 
-  <div class="form-group">
-    <label class="control-label requiredField" for="email">Email<span class="asteriskField">*</span></label>
-    <input class="form-control" id="email" name="email" type="text"/>
-  </div>
+          <div class="form-group">
+            <label class="control-label requiredField" for="email">Email<span class="asteriskField">*</span></label>
+            <input class="form-control" id="email" name="email" type="text"/>
+          </div>
 
-  <div class="form-group">
-    <label class="control-label " for="subject">Subject</label>
-    <input class="form-control" id="subject" name="subject" type="text"/>
-  </div>
+          <div class="form-group">
+            <label class="control-label " for="subject">Subject</label>
+            <input class="form-control" id="subject" name="subject" type="text"/>
+          </div>
 
-  <div class="form-group">
-    <label class="control-label " for="message">Message</label>
-    <textarea class="form-control" cols="40" id="message" name="message" rows="10"></textarea>
-  </div>
+          <div class="form-group">
+            <label class="control-label " for="message">Message</label>
+            <textarea class="form-control" cols="40" id="message" name="message" rows="10"></textarea>
+          </div>
 
-  <div class="form-group">
-    <button class="btn btn-primary " name="submit" type="submit">Submit</button>
-  </div>
+          <div class="form-group">
+            <button class="btn btn-primary " name="submit" type="submit">Submit</button>
+          </div>
+        </form>
 
-</form>
       </div>
     )
   }
